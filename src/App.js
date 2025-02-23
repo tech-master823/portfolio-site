3c1443097fd344db823df759df2232ecb8018f55
tech-master823 <carlsh0823@gmail.com>
2025-02-23 08:37:03 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -29,13 +29,14 @@ const App = () => {
   useEffect(() => {
     setFirebaseData({
       Bio: {
-        name: "Gustavo Oliveira P",
-        roles: "Python & AI Specialist",
+        name: "Gustavo Pacheco",
+        roles: "Senior Full Stack Developer",
         resume: "https://app.enhancv.com/share/56d521b5/?utm_medium=growth&utm_campaign=share-resume&utm_source=dynamic",
         linkedin: "https://www.linkedin.com/in/gustavo-pacheco28/",
         github: "https://github.com/tech-master823/", 
-        description: "Senior Python Developer with expertise in robust web applications and AI solutions. " + 
-        "Proficient in Django, Flask, and modern frontend frameworks like React and Vue.js. " + 
+        description: "Senior Full Stack Developer with expertise in robust web applications and AI solutions. " + 
+        "Worked on E-Commerce, Saas, Healthcare, etc with Proficiency in Django, FastAPI, Node.js, Spring Boot, ASP.NET, Laravel," + 
+        " and modern frontend frameworks like React, Angular and Vue.js. " + 
         "Experienced in delivering scalable, secure full-stack apps and deploying on AWS, Azure, and GCP. " + 
         "Contributed to the LangChain framework and created various GPT-based applications.",
       },
@@ -44,26 +45,33 @@ const App = () => {
           title: "Generative AI",
           skills: [
             { name: "OpenAI", image: "/icons/chatgpt-svg.svg" }, 
-            { name: "Langchain", image: "/icons/chain-image.png" },
-            { name: "LLaMA", image: "/icons/llama-image.png" },
-            { name: "RAG System", image: "/icons/setting-svg.svg" },
             { name: "NLP", image: "/icons/nlp-svg.svg" },
+            { name: "Langchain", image: "/icons/chain-image.png" },
+            { name: "LlamaIndex", image: "/icons/llama-image.png" },
+            { name: "RAG System", image: "/icons/setting-svg.svg" },
             { name: "Stable Diffusion", image: "/icons/stable-image.png" },
+            { name: "Pinecone", image: "/icons/pinecone.svg" },
           ]
         },
         {
-          title: "AI/ML Tools",
+          title: "Programming Languages",
           skills: [
             { name: "Python", image: "/icons/python-svg.svg" }, 
-            { name: "Tensorflow", image: "/icons/tensorflow-svg.svg" },
-            { name: "Numpy", image: "/icons/numpy-svg.svg" },
-            { name: "Pandas", image: "/icons/pandas-svg.svg" },
-            { name: "PyTorch", image: "/icons/pytorch-svg.svg" },
+            { name: "C#", image: "/icons/csharp.svg" },
+            { name: ".NET", image: "/icons/dotnet.svg" },
+            { name: "Javascript", image: "/icons/javascript.svg" },
+            { name: "Typescript", image: "/icons/typescript.svg" },
+            { name: "PHP", image: "/icons/php.svg" },
+            { name: "Java", image: "/icons/java.svg" },
+            { name: "R", image: "/icons/r.svg" },
+            { name: "SQL", image: "/icons/sql.svg" },
           ]
         },
         {
           title: "Frontend",
           skills: [
+            { name: "HTML5", image: "/icons/html-5-svg.svg" }, 
+            { name: "CSS3", image: "/icons/CSS-svg.svg" }, 
             { name: "React", image: "/icons/react-svg.svg" }, 
             { name: "Next.js", image: "/icons/nextjs-svg.svg" }, 
             { name: "Vue.js", image: "/icons/vue-svg.svg" },
@@ -75,28 +83,34 @@ const App = () => {
         {
           title: "Backend",
           skills: [
-            { name: "Python", image: "/icons/python-svg.svg" }, 
             { name: "Django", image: "/icons/django-svg.svg" },
             { name: "Flask", image: "https://repository-images.githubusercontent.com/596892/cc2c69ec-9251-4b33-8283-b86a8659c9cb" },
             { name: "FastAPI", image: "/icons/fastapi-image.png" },
-            { name: "NodeJS", image: "/icons/node-svg.svg" },
+            { name: "ASP.NET", image: "/icons/asp.net.png" },
+            { name: "Spring", image: "/icons/spring.png" },
+            { name: "Node.js", image: "/icons/node-svg.svg" },
             { name: "Express", image: "/icons/express-svg.svg" },
+            { name: "Nest.js", image: "/icons/nest.svg" },
+            { name: "Laravel", image: "/icons/laravel.svg" },
           ]
         },
         {
           title: "Database & Clouding",
           skills: [
-            { name: "Azure", image: "/icons/azure-svg.svg" },
-            { name: "AWS", image: "/icons/aws-svg.svg" },
-            { name: "GCP", image: "/icons/google-cloud-svg.svg" },
-            { name: "Firebase", image: "/icons/firebase-svg.svg" },
-            { name: "PostgreSQL", image: "/icons/postgresql-svg.svg" },
             { name: "MySQL", image: "/icons/mysql-svg.svg" },
+            { name: "PostgreSQL", image: "/icons/postgresql-svg.svg" },
+            { name: "Firebase", image: "/icons/firebase-svg.svg" },
+            { name: "MS SQL Server", image: "/icons/sql-server.svg" },
+            { name: "Redis", image: "/icons/redis.svg" },
+            { name: "AWS", image: "/icons/aws-svg.svg" },
+            { name: "Azure", image: "/icons/azure-svg.svg" },
+            { name: "GCP", image: "/icons/google-cloud-svg.svg" },
           ]
         },
         {
           title: "DevOps & Tools",
           skills: [
+            { name: "Visual Studio", image: "/icons/visual-studio.svg" }, 
             { name: "Docker", image: "/icons/docker-svg.svg" },
             { name: "Git", image: "/icons/git-svg.svg" },
             { name: "Kubernetes", image: "/icons/kubernetes-svg.svg" },
@@ -140,40 +154,56 @@ const App = () => {
         {
           id: "1",
           company: "IConflux Technologies Pvt. Ltd.",
-          role: "Senior Python Developer – Full-Stack, AI & Cloud Solutions",
+          role: "Senior Full Stack Developer – AI & Cloud Solutions",
           img: "/icons/iconflux-company-logo.png",
           desc: "Actively engaged in developing innovative AI services, " + 
           "including a healthcare chatbot and LLM-powered microservices, " + 
           "with a focus on enhancing data management and user satisfaction through cloud platforms.",
           date: "Mar 2020 - Present",
-          skills: ["OpenAI", "GPT", "Langchain", "LLaMA 3", "AWS", "Azure", "RAG", "Next.js"]
+          skills: ["Django", "FastAPI", "ASP.NET", "Spring Boot", "Nest.js", "R", "OpenAI", "GPT", "Langchain", "LLaMA 3", "AWS", "Azure", "Docker"]
         },
         {
           id: "2",
           company: "Synodus",
-          role: "Full Stack & AI Developer",
+          role: "Full Stack Developer",
           img: "/icons/synodus-company-logo.jpg",
           desc: "Contributed to Architect and develop multiple web applications, " + 
           "including an ERP system and a scalable e-commerce platform utilzing innovative AI strategies, " + 
           "while establishing CI/CD pipelines with Docker and Kubernetes, resulting in reduced project deployment time, and enhanced productivity and quality of code.",
           date: "Jan 2016 – Dec 2019",
-          skills: ["Django", "Flask", "FastAPI", "React", "Vue.js", "OpenAI", "Docker", "Kubernetes"]
+          skills: ["Django", "Spring Boot", "ASP.NET", "Laravel", "React", "Vue.js", "Angular", "OpenAI", "Docker", "Kubernetes"]
         }, 
         {
           id: "3",
           company: "CodLUCK Technology ., JSC",
-          role: "Python Developer",
+          role: "Software Developer",
           img: "/icons/codluck-company-logo.jpg",
           desc: "Enhanced data accuracy, processing speed, and revenue growth by spearheading innovative data solutions, " + 
-          "including a sophisticated Python web scraper, efficient data pipelines, and a predictive model for client churn that transformed business strategies.",
+          "including a sophisticated web scraper, efficient data pipelines, and a predictive model for client churn that transformed business strategies.",
           date: "Jan 2013 – Nov 2015",
-          skills: ["Django", "Tensorflow", "Pandas", "Numpy", "Docker"]
+          skills: ["Django", "Flask", "Node.js", "Laravel", "Java", "Tensorflow", "Pandas", "Numpy", "Docker"]
         }
       ],
       projects: [
         {
           id: "1",
           ontop: 1,
+          image: "/image/gavani.webp",
+          title: "Gavani - eCommerce for Clothes sales",
+          description: "Gavani is a platform for selling clothes online. " + 
+          "This is for Sellers and buyers. The platform does provide other automation settings and tools to help user find potential buyer or seller.",
+          responsibilities: [
+            "Create a responsive UI with Vue.js, develop custom APIs in Laravel and integrate third-party services like inventory management and payment gateways.",
+            "Implement secure payments, comply with data privacy laws, and establish user authentication to protect sensitive information.",
+            "Kept providing data privately to each user, while focusing on privacy by utilizing (Django RESTful Framework)DRF authentication.",
+          ],
+          tags: ["Vue.js", "Tailwind CSS", "Laravel", "PostgreSQL"],
+          date: "Feb 2017 - Sept 2017",
+          webapp: "https://gavani.com",
+        },
+        {
+          id: "9",
+          ontop: false,
           image: "/image/erp.png",
           title: "ERPNext - ERP System",
           description: "Leveraged full stack development in our ERP system to automate data entry, optimize supply chain management, and forecast inventory needs through predictive analytics. " + 
@@ -200,7 +230,7 @@ const App = () => {
             "Extract key informations from existing e-commerce system, and store it to Vector Database - Pinecone for trainning LLM.",
             "Established RAG system for integrating AI chatbot to platform, by uilizing  Langchain, GPT model and LLaMA."
           ],
-          tags: ["React.js", "Firebase", "RAG", "GPT", "Pinecone", "Langchain", "LLaMA"],
+          tags: ["Django", "React.js", "Firebase", "RAG", "GPT", "Pinecone", "Langchain", "LLaMA"],
           date: "Feb 2018 - Nov 2018",
           webapp: "https://www.fahertybrand.com"
         }, 
@@ -208,7 +238,7 @@ const App = () => {
           id: "2",
           ontop: 1,
           image: "/image/healthcare.png",
-          title: "Health.ai - Healthcare AI Assistant",
+          title: "Health.ai - AI-Powered Healthcare Consulting System",
           description: "Innovative AI Assistant system be able to medical consultant with patient based on rich expert medical knowledge. " + 
           "Simulating a dialogue with the patient can offer tailored health advice, suggest diagnoses, " + 
           "and recommend actions based on their specific symptoms and medical history.",
@@ -217,19 +247,20 @@ const App = () => {
             "Implemented entire RAG architecture for AI healthcare assistant system, and embeded AI Speech-To-Text and Text-To-Speech feature.",
             "Established large amount of meidcal knowledge base, and trained GPT model with it, by utilizing Langchain, LLaMA and Vector DB(Pinecone).",
           ],
-          tags: ["Langchain", "ChatGPT", "LLaMA", "RAG", "Pinecone", "Next.js"],
+          tags: ["FastAPI", "Langchain", "ChatGPT", "LLaMA", "RAG", "Pinecone", "Next.js"],
           date: "Nov 2021 - Apr 2022",
+          webapp: "https://health.ai",
           github: "https://github.com/tech-master823/Healthcare-consulting-system"
-        }, 
+        },
         {
           id: "3",
           ontop: 1,
           image: "/image/image-generator.png",
-          title: "AI Image Generator - GAN",
+          title: "AI-Powered Art Generation Platform",
           description: "Developed and launched a cutting-edge AI Art Service platform utilizing the Stable Diffusion model and GANs, " + 
           "allowing users to effortlessly do anything related to smart art with simple text prompts and unlocking unparalleled creative possibilities " + 
           "for digital art, advertising, and marketing.",
-          tags: ["Python", "OpenAI", "Stable Diffusion", "GAN", "Machine Learning", "Next.js"],
+          tags: ["Nest.js", "React", "OpenAI", "Stable Diffusion", "GAN"],
           responsibilities: [
             "Made sure detailed requirements of entire project, and designed seamless and user-friendly frontend with Next.js",
             "Built Python based backend - Django for integrating Stable Diffusion model, and made RESTful API for communicating between frontend and backend.",
@@ -284,7 +315,7 @@ const App = () => {
             "Based on detailed requirements, Improved editing and refining Claim Notice by integrating AI Copilot to Kyber platform.",
             "Completed various innovative features, including choosing Claim template, pre-fill information based on previous information.",
           ],
-          tags: ["Langchain", "ChatGPT", "LLaMA", "RAG", "Pinecone", "React", "MongoDB"],
+          tags: ["Langchain", "ChatGPT", "Pinecone", "React", "ASP.NET Core", "MongoDB"],
           date: "Nov 2021 - Apr 2022",
           webapp: "https://www.askkyber.com/"
         },
@@ -325,7 +356,7 @@ const App = () => {
           img: "/icons/can-tho-university.png",
           date: "Mar 2010 – Dec 2014",
           grade: "97.76%",
-          desc: "I gained in-depth knowledge of programming languages like C++, Python, and JavaScript, " + 
+          desc: "I gained in-depth knowledge of programming languages like C++, Python, PHP, C#, Java and JavaScript, " + 
           "dedicating significant effort to mastering Python and its scientific libraries, " + 
           "while also exploring backend technologies like Django and Flask to enhance my web development skills."
         },
